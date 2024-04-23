b6d56d2263f8fef6a02c47d7280e52c1c68ab6ad
Nicolas Kevin <143231987+NicolasKevin2@users.noreply.github.com>
2024-04-23 20:25:16 -0300

    Add files via upload

---
+++ prova_lp/src/prova_lp/Main.java
@@ -0,0 +1,15 @@
+package prova_lp;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bem-vindo ao paredão do BBB!");
+        System.out.println("Em quem você vota para sair");
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
---
+++ prova_lp/src/prova_lp/jogador.java
@@ -0,0 +1,36 @@
+package prova_lp;
+
+import java.util.ArrayList;
+
+public class jogador {
+{
+
+  ArrayList <jogador> jogadors = new ArrayList<jogador>(){
+        jogador = new jogador("Alane Dias", 1);
+        jogador =new jogador("Beatriz reis", 2);
+        jogador = new jogador("Davi Brito", 3);
+        jogador = new jogador("Deniziane Ferreira",4);
+        jogador = new jogador("Feranda Bande",5);
+        jogador = new jogador("Giovanna Lima",6);
+        jogador = new jogador("Giovanna Pitel",7);
+        jogador = new jogador("Isabelle nogueira",8);
+        jogador = new jogador("Juninho",9);
+        jogador = new jogador("Leidy eline",10);
+        jogador = new jogador("Lucas henrique",11);
+        jogador = new jogador("Lucas Luigi",12);
+        jogador = new jogador("Lucas pizane",13);
+        jogador = new jogador("Marcos Vinícius",14);
+        jogador = new jogador("Matteus Amaral",15);
+        jogador = new jogador("Maycon Cosmer",16);
+        jogador = new jogador("Mc Bin Laden",17);
+        jogador = new jogador("Michel Nogueira",18);
+        jogador = new jogador("Nizam",19);
+        jogador = new jogador("Raquele Cardoso",20);
+        jogador = new jogador("Rodriguinho",21);
+        jogador = new jogador("Thalyta Alves",22);
+        jogador = new jogador("Vanessa Lopes",23);
+        jogador = new jogador("Vinicius Rodrigues",24);
+        jogador = new jogador("Wanessa Camargo",25);
+        jogador = new jogador("Yasmin Brunet",26);
+
+}
